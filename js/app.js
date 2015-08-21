d5b1af1cfc401638dd16912f2900c28bae6ac6e6
Benom84 <menahem.ben@gmail.com>
2015-08-21 11:20:40 +0300

    Skeleton is ready
    Things to do:
    Select to appropriate list (Tremper/Trempist)
    Post new offers/requests to server
    Edit UI

--- js/app.js
+++ js/app.js
@@ -16,6 +16,14 @@ myApp.config(['$routeProvider', function ($routeProvider) {
             templateUrl: 'partials/details.html',
             controller: 'DetailsController'
     }).
+        when('/details_tremper/:itemId' ,{
+            templateUrl: 'partials/details_tremper.html',
+            controller: 'TremperDetailsController'
+        }).
+        when('/details_trempist/:itemId' ,{
+            templateUrl: 'partials/details_trempist.html',
+            controller: 'TrempistDetailsController'
+        }).
         when('/', {
             templateUrl: 'partials/login.html',
             controller: 'UserDetailsController'
@@ -24,7 +32,28 @@ myApp.config(['$routeProvider', function ($routeProvider) {
             templateUrl: 'partials/typeSelect.html',
             controller: 'UserDetailsController'
         }).
+        when('/trempist', {
+            templateUrl: 'partials/trempist.html',
+            controller: 'TrempistController'
+        }).
+        when('/tremper', {
+            templateUrl: 'partials/tremper.html',
+            controller: 'TremperController'
+        }).
         otherwise({
             redirectTo: '/list'
     });
 }]);
+
+myApp.user = {
+    name : '',
+    phone : '',
+    gender : '',
+    exists : false
+};
+
+myApp.factory('Data', function() {
+        return myApp.user;
+    }
+
+);
--- js/controllers.js
+++ js/controllers.js
@@ -16,12 +16,50 @@ tremperControllers.controller('DetailsController', ['$scope', '$http', '$routePa
     });
 }]);
 
-tremperControllers.controller('UserDetailsController', ['$scope', '$http', '$location' ,function ($scope, $http, $location) {
-    $scope.user = {
-        name: '',
-        phone: '',
-        gender: ''
+tremperControllers.controller('TremperDetailsController', ['$scope', '$http', '$routeParams' ,function ($scope, $http, $routeParams) {
+    $http.get('js/data.json').success(function(data) {
+        $scope.trempers = data;
+        $scope.whichItem = $routeParams.itemId;
+    });
+}]);
+
+tremperControllers.controller('TrempistDetailsController', ['$scope', '$http', '$routeParams' ,function ($scope, $http, $routeParams) {
+    $http.get('js/data.json').success(function(data) {
+        $scope.trempists = data;
+        $scope.whichItem = $routeParams.itemId;
+    });
+}]);
+
+tremperControllers.controller('UserDetailsController', ['$scope', '$http', '$location', 'Data' ,function ($scope, $http, $location, Data) {
+
+    $scope.user = Data;
+
+    if (localStorage.getItem("userDetailsSet") != null) {
+        $scope.user.name = localStorage.getItem("userName");
+        $scope.user.phone = localStorage.getItem("userPhone");
+        $scope.user.gender = localStorage.getItem("userGender");
+        $scope.user.exists = true;
+        Data.name =  $scope.user.name;
+        Data.phone = $scope.user.phone;
+        Data.gender = $scope.user.gender;
+        Data.exists = true;
+        //window.alert("Details found in memory");
+        //$scope.updateUser($scope.user);
+
     };
+
+//    if (!$scope.user.exists) {
+//        $scope.user = {
+//            name: '',
+//            phone: '',
+//            gender: ''
+//        };
+//        window.alert("user was undefined");
+//
+//    } else {
+//
+//    }
+
 
     $scope.updateUser = function(user, fromMain) {
         window.alert("UpdateUser invoked");
@@ -34,18 +72,83 @@ tremperControllers.controller('UserDetailsController', ['$scope', '$http', '$loc
         };
 
     }
+}]);
+
+tremperControllers.controller('TremperController', ['$scope', '$http', '$routeParams', 'Data' ,function ($scope, $http, $routeParams, Data) {
+
+    $http.get('js/data.json').success(function(data) {
+        $scope.trempists = data;
 
 
-    if (localStorage.getItem("userDetailsSet") != null) {
-        $scope.user.name = localStorage.getItem("userName");
-        $scope.user.phone = localStorage.getItem("userPhone");
-        $scope.user.gender = localStorage.getItem("userGender");
-        //window.alert("Details found in memory");
-        //$scope.updateUser($scope.user);
+        $scope.whichItem = $routeParams.itemId;
+    });
+
+    $scope.user = Data;
+
+    $scope.datetime = new Date();
+
+    $scope.myTremper = {
+        name : $scope.user.name,
+        gender : $scope.user.gender,
+        phone : $scope.user.phone,
+        from : '',
+        to : '',
+        details : '',
+        type : 'tremper',
+        hour : $scope.datetime.getHours(),
+        minutes : $scope.datetime.getMinutes(),
+        date : $scope.datetime,
+        time : ''
+    }
+
+    $scope.updateTremperList = function(myTremper) {
+        myTremper.time = new Date(myTremper.date.getFullYear(), myTremper.date.getMonth(), myTremper.date.getDate(), myTremper.hour, myTremper.minutes, 0,0);
+        if (myTremper.time < $scope.datetime) {
+            window.alert("You cannot add a tremp in the past \n(Until further development in quantum physics)");
+        }
+        else {
+            window.alert(myTremper.time);
+        }
 
     };
+}]);
+
+tremperControllers.controller('TrempistController', ['$scope', '$http', '$routeParams', 'Data' ,function ($scope, $http, $routeParams, Data) {
+
+    $http.get('js/data.json').success(function(data) {
+        $scope.trempers = data;
 
 
+        $scope.whichItem = $routeParams.itemId;
+    });
 
+    $scope.user = Data;
+
+    $scope.datetime = new Date();
+
+    $scope.myTrempist = {
+        name : $scope.user.name,
+        gender : $scope.user.gender,
+        phone : $scope.user.phone,
+        from : '',
+        to : '',
+        details : '',
+        type : 'trempist',
+        hour : $scope.datetime.getHours(),
+        minutes : $scope.datetime.getMinutes(),
+        date : $scope.datetime,
+        time : ''
+    }
+
+    $scope.updateTrempistsList = function(myTrempist) {
+        myTrempist.time = new Date(myTrempist.date.getFullYear(), myTrempist.date.getMonth(), myTrempist.date.getDate(), myTrempist.hour, myTrempist.minutes, 0,0);
+        if (myTrempist.time < $scope.datetime) {
+            window.alert("You cannot request a tremp in the past \n(Until further development in quantum physics)");
+        }
+        else {
+            window.alert(myTrempist.time);
+        }
+
+    };
 }]);
 
